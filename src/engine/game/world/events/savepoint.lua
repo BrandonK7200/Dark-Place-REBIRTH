204daa4503ff47eddf0bf9cb1906dc6684a90839
AcousticJamm <jasmicheals@gmail.com>
2024-08-21 09:51:40 -0400

    Weird route savepoints

--- src/engine/game/world/events/savepoint.lua
+++ src/engine/game/world/events/savepoint.lua
@@ -24,9 +24,28 @@ function Savepoint:init(x, y, properties)
     -- So divide by 2, round, then multiply by 2 to get the right size for 2x.
     local width, height = self:getSize()
     self:setHitbox(0, math.ceil(height / 4) * 2, width, math.floor(height / 4) * 2)
+
+	self.override_power = properties["override_power"]
 end
 
 function Savepoint:onInteract(player, dir)
+	if Game:getFlag("weird") and (self.text and #self.text > 0) and not self.override_power then
+		local party_text = ""
+		for i,member in ipairs(Game.party) do
+			party_text = party_text .. member.name
+			if i < #Game.party-1 then
+				party_text = party_text ..",[wait:2] "
+			elseif i == #Game.party-1 then
+				party_text = party_text .." and "
+			end
+		end
+		local verb = #Game.party == 1 and "is" or "are"
+		if Game:hasPartyMember("jamm") and Game:getPartyMember("jamm").name == "J+M" then
+			verb = "are"
+		end
+		self.text = {"* " .. party_text .. " "..verb.." filled with power."}
+	end
+
     Assets.playSound("power")
 
     if self.text_once and self.used then
