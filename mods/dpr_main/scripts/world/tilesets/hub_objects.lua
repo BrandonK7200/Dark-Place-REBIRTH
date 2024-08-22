43e876320324a9dd12e0d30882a928c6d2f191f8
BrandonK7200 <brandonkillman@hotmail.com>
2024-08-21 22:40:56 -0300

    Main Hub (WIP)

---
+++ mods/dpr_main/scripts/world/tilesets/hub_objects.lua
@@ -0,0 +1,41 @@
+return {
+  version = "1.9",
+  luaversion = "5.1",
+  tiledversion = "1.9.0",
+  name = "hub_objects",
+  class = "",
+  tilewidth = 180,
+  tileheight = 140,
+  spacing = 0,
+  margin = 0,
+  columns = 0,
+  objectalignment = "unspecified",
+  tilerendersize = "tile",
+  fillmode = "stretch",
+  tileoffset = {
+    x = 0,
+    y = 0
+  },
+  grid = {
+    orientation = "orthogonal",
+    width = 1,
+    height = 1
+  },
+  properties = {},
+  wangsets = {},
+  tilecount = 2,
+  tiles = {
+    {
+      id = 0,
+      image = "../../../assets/sprites/tileobjects/hub_deltarune.png",
+      width = 120,
+      height = 80
+    },
+    {
+      id = 1,
+      image = "../../../assets/sprites/tileobjects/aroombetween.png",
+      width = 180,
+      height = 140
+    }
+  }
+}
---
+++ mods/dpr_main/scripts/world/tilesets/main_hub.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.0" name="main_hub" tilewidth="40" tileheight="40" tilecount="100" columns="10">
+ <image source="../../../assets/sprites/tilesets/main_hub.png" width="400" height="400"/>
+</tileset>
