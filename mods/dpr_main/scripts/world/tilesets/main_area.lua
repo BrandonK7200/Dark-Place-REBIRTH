bcb156930c1c7e8b7a72e0d8cde3004e0b879d01
JustAnotherRandomGithubUser <125609186+JustAnotherRandomGithubUser@users.noreply.github.com>
2024-08-22 00:51:41 -0400

    Added/changed some stuff.
    - Main Hub now uses a new (WIP) tileset.
    - Hero has downwards walking sprites now.
    - Removed hub_objects.tsx.

---
+++ mods/dpr_main/scripts/world/tilesets/main_area.lua
@@ -0,0 +1,31 @@
+return {
+  version = "1.9",
+  luaversion = "5.1",
+  tiledversion = "1.9.0",
+  name = "main_area",
+  class = "",
+  tilewidth = 40,
+  tileheight = 40,
+  spacing = 0,
+  margin = 0,
+  columns = 13,
+  image = "../../../assets/sprites/tilesets/main_area.png",
+  imagewidth = 520,
+  imageheight = 600,
+  objectalignment = "unspecified",
+  tilerendersize = "tile",
+  fillmode = "stretch",
+  tileoffset = {
+    x = 0,
+    y = 0
+  },
+  grid = {
+    orientation = "orthogonal",
+    width = 40,
+    height = 40
+  },
+  properties = {},
+  wangsets = {},
+  tilecount = 195,
+  tiles = {}
+}
---
+++ mods/dpr_main/scripts/world/tilesets/main_area.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.0" name="main_area" tilewidth="40" tileheight="40" tilecount="195" columns="13">
+ <editorsettings>
+  <export target="main_area.lua" format="lua"/>
+ </editorsettings>
+ <image source="../../../assets/sprites/tilesets/main_area.png" width="520" height="600"/>
+</tileset>
