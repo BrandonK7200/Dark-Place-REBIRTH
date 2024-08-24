9f51716f29f5f374b9c36af1f4e285f94fd6327e
AcousticJamm <jasmicheals@gmail.com>
2024-08-24 17:16:53 -0400

    Menu fix

--- src/engine/game/world/ui/dark/darkpowermenu.lua
+++ src/engine/game/world/ui/dark/darkpowermenu.lua
@@ -439,10 +439,10 @@ function DarkPowerMenu:drawExperience()
     love.graphics.print( "NEXT:",   242-6,  190)
     love.graphics.print("KILLS:",   242-6,  224)
 
-    love.graphics.print(               self:getLOVE(),  242+64, 122)
-    love.graphics.print(                self:getExp(),  242+49, 156)
-    love.graphics.print(             self:getNextLv(),  242+62, 190)
-    love.graphics.print(Game:getFlag("library_kills"),  242+76, 224)
+    love.graphics.print(               self:getLOVE(),  	242+64, 122)
+    love.graphics.print(                self:getExp(),  	242+49, 156)
+    love.graphics.print(             self:getNextLv(),  	242+62, 190)
+    love.graphics.print(Game:getFlag("library_kills", 0),  	242+76, 224)
 end
 
 function DarkPowerMenu:drawCombos()
